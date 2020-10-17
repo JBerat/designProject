5dbf301f1bd47d61ca68be33366d7b04c7b6e7c5
JBerat <berat.99@hotmail.com>
2020-10-17 11:24:51 +0300

    forgot password updated

--- app/src/main/java/com/beratucgul/designexample/MainActivity.java
+++ app/src/main/java/com/beratucgul/designexample/MainActivity.java
@@ -8,14 +8,17 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.FileObserver;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button signIn, forgotPassword, dontHaveAccount, signinGoogle, signinApple;
     EditText username, password;
+    EditText ForgotEmailEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void forgotPassword(View view) {
 
-        AlertDialog.Builder alert =     new AlertDialog.Builder(this);
+       /* AlertDialog.Builder alert =     new AlertDialog.Builder(this);
         alert.setTitle("Forgot Password")
                 .setMessage("No worries, let us help you out!");
 
@@ -58,6 +61,14 @@ public class MainActivity extends AppCompatActivity {
         });
         alert.show();
 
+        */
+
+
+        ShowDialog();
+
+
+
+
 
     }
 
@@ -66,14 +77,31 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-  /*  private boolean loadFragment(Fragment fragment) {
-        if(fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id)
-        }
+    public void ShowDialog(){
+        final AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).create();
+
+        LayoutInflater layoutInflater = this.getLayoutInflater();
+        View dialogView = layoutInflater.inflate(R.layout.custom_dialog_layout, null);
+
+        final EditText editText = dialogView.findViewById(R.id.ForgotEmailEditText);
+
+        Button Continue = dialogView.findViewById(R.id.ForgotEmailContinueButton);
+
+        Continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        dialog.setView(dialogView);
+        dialog.show();
+
     }
-    
-   */
+
 
 }
